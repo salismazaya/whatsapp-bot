905642acec82f8ebf2655fcb125f2a78c403a1c9
Salis Mazaya <salismazaya@gmail.com>
2021-02-06 15:45:21 +0700

    make it better

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // https://github.com/salismazaya/whatsapp-bot
+// Jika ingin mengubah / mengedit, mohon untuk tidak menghilangkan link github asli di dalam bot terimakasih ^_^
 
 const fs = require("fs");
 const axios = require("axios");
 const fstring = require("sprintf-js").sprintf;
 const webp = require('webp-converter');
 const PDFDocument = require("pdfkit");
+const brainly = require("brainly-scraper");
 const { MessageType, Mimetype } = require("@adiwajshing/baileys");
 const { conn } = require("./conn.js");
-const { texts } = require("./text.js");
 
 if (!fs.existsSync(".temp")) fs.mkdirSync(".temp");
 
@@ -40,8 +41,8 @@ async function main() {
 			if (message.message.conversation == "!done" || bufferImagesForPdf[senderNumber].length > 19) {
 				const pdf = new PDFDocument({ autoFirstPage:false });
 				const bufferImages = bufferImagesForPdf[senderNumber];
-				for (let i = 0; i < bufferImages.length; i++) {
-					const image = pdf.openImage(bufferImages[i]);
+				for (const bufferImage of bufferImages) {
+					const image = pdf.openImage(bufferImage);
 					pdf.addPage({ size:[image.width, image.height] });
 					pdf.image(image, 0, 0);
 				}
@@ -70,9 +71,41 @@ async function main() {
 			}
 
 
-		} else if (message.message.conversation == "!command") {
-			conn.sendMessage(senderNumber, fstring(texts.command, conn.user.name), MessageType.text, { quoted: message })
+		} else if (message.message.conversation == "!help") {
+			const text = `Halo kak selamat datang di *${conn.user.name}*!
+
+- kirim *!help* untuk melihat daftar perintah dari bot ini
+
+- kirim *!contact* untuk menghubungi pembuat bot
+
+- kirim gambar dengan caption *!sticker* untuk membuat sticker
+
+- kirim *!pdf* untuk membuat pdf dari gambar
+
+- reply sticker dengan caption *!toimg* untuk membuat sticker ke gambar
+
+- kirim *!write "masukan text disini"* untuk menulis ke kertas
+
+- kirim *!brainly "pertanyaan kamu"* untuk mencari pertanyaan dan jawaban di brainly
+
+Bot sensitif terhadap simbol / spasi / huruf kecil / huruf besar jadi, bot tidak akan membalas jika terjadi kesalahan penulisan!
+
+Bot ini open source loh! kakak bisa cek di https://github.com/salismazaya/whatsapp-bot
+
+apa? mau traktir aku? boleh banget https://saweria.co/salismazaya`
+
+			conn.sendMessage(senderNumber, text, MessageType.text, { quoted: message })
 		
+		} else if (message.message.conversation == "!contact") {
+			const text = `Hubungi saya di
+
+- Whatsapp: wa.me/6283154881466
+- Facebook: fb.me/salismazaya
+- Telegram: t.me/salismiftah
+- Email: salismazaya@gmail.com`
+			conn.sendMessage(senderNumber, text, MessageType.text, { quoted: message })
+
+
 		} else if (message.message.imageMessage && message.message.imageMessage.caption == "!sticker" && message.message.imageMessage.mimetype == "image/jpeg") {
 			const pathFile = ".temp/" + Math.floor(Math.random() * 1000000 + 1) + ".jpg";
 			fs.writeFileSync(pathFile, await conn.downloadMediaMessage(message));
@@ -102,9 +135,9 @@ async function main() {
 			const data = await axios.post("https://salism3.pythonanywhere.com/write", { "text": message.message.conversation.replace("!write ", "") });
 			const imagesUrl = data.data.images.slice(0, 4);
 
-			for (let i = 0; i < imagesUrl.length; i++) {
+			for (const imageUrl of imagesUrl) {
 				const response = await axios({
-					url: imagesUrl[i],
+					url: imageUrl,
 					method: "GET",
 					responseType: "arraybuffer",
 				});
@@ -118,7 +151,15 @@ async function main() {
 
 			conn.sendMessage(senderNumber, "Silahkan kirim gambarnya satu persatu! jangan spam ya!", MessageType.text, { quoted: message })
 
-		} 
+		} else if (typeof(message.message.conversation) == "string" && message.message.conversation.startsWith("!brainly ")) {
+			const data = await brainly(message.message.conversation.replace("!brainly ", ""));
+			if (data.success) {
+				for (const question of data.data.slice(0, 3)) {
+					const text = `*Pertanyaan:* ${question.pertanyaan.trim()}\n\n*Jawaban*: ${question.jawaban[0].text.replace("Jawaban:", "").trim()}`
+					await conn.sendMessage(senderNumber, text, MessageType.text, { quoted: message })
+				}
+			}
+		}
 
 		
 	});
